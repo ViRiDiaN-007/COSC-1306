4b8bc40e9076dbe1715a0913bca8a3dbc8a48501
ViRiDiaN-007 <42155675+ViRiDiaN-007@users.noreply.github.com>
2020-03-06 01:46:20 -0600

    Add files via upload

---
+++ Assignment 1/Assignment 1.py
@@ -0,0 +1,13 @@
+##Assignment 1
+
+
+##Name:
+##Email:
+##Date:
+
+
+print('This is my first Python program.')
+#this is a comment
+name = input("What's your name? ")
+print(name + ', you will like Python')
+input()
